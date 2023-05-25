c53e0bcdc14fc5aeef222fa54f3f1cfa80be1d39
Wesley Kanated <105467642+KANATED@users.noreply.github.com>
2023-05-25 04:44:31 +0100

    Update getLine.c

--- getLine.c
+++ getLine.c
@@ -1,10 +1,10 @@
 #include "shell.h"
 
 /**
- * input_buf - buffers chained commands
- * @info: parameter struct
- * @buf: address of buffer
- * @len: address of len var
+ *_func - buffers chained commands
+ * info: parameter struct
+ * buf: address of buffer
+ * len: address of len var
  *
  * Return: bytes read
  */
@@ -45,8 +45,8 @@ ssize_t input_buf(info_t *info, char **buf, size_t *len)
 }
 
 /**
- * get_input - gets a line minus the newline
- * @info: parameter struct
+ * _func - gets a line minus the newline
+ * info: parameter struct
  *
  * Return: bytes read
  */
@@ -90,10 +90,10 @@ ssize_t get_input(info_t *info)
 }
 
 /**
- * read_buf - reads a buffer
- * @info: parameter struct
- * @buf: buffer
- * @i: size
+ * _func - reads a buffer
+ * info: parameter struct
+ * buf: buffer
+ * i: size
  *
  * Return: r
  */
@@ -110,10 +110,10 @@ ssize_t read_buf(info_t *info, char *buf, size_t *i)
 }
 
 /**
- * _getline - gets the next line of input from STDIN
- * @info: parameter struct
- * @ptr: address of pointer to buffer, preallocated or NULL
- * @length: size of preallocated ptr buffer if not NULL
+ * _func- gets next line of input from STDIN
+ * info: parameter struct
+ * ptr: address of pointer to buffer, preallocated or NULL
+ * length: size of preallocated ptr buffer if not NULL
  *
  * Return: s
  */
@@ -157,8 +157,8 @@ int _getline(info_t *info, char **ptr, size_t *length)
 }
 
 /**
- * sigintHandler - blocks ctrl-C
- * @sig_num: the signal number
+ * _func - blocks ctrl-C
+ * sig_num: the signal number
  *
  * Return: void
  */
