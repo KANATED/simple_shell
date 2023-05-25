953c94b7665367e4541da0f6f5da97fd00e397d2
Wesley Kanated <105467642+KANATED@users.noreply.github.com>
2023-05-25 05:25:09 +0100

    Update string.c

--- string.c
+++ string.c
@@ -1,8 +1,8 @@
 #include "shell.h"
 
 /**
- * _strlen - returns the length of a string
- * @s: the string whose length to check
+ * _func - returns the length of a string
+ * s: the string whose length to check
  *
  * Return: integer length of string
  */
@@ -19,9 +19,9 @@ int _strlen(char *s)
 }
 
 /**
- * _strcmp - performs lexicogarphic comparison of two strangs.
- * @s1: the first strang
- * @s2: the second strang
+ * _func- performs lexicogarphic comparison of two strangs.
+ * s1: the first strang
+ * s2: the second strang
  *
  * Return: negative if s1 < s2, positive if s1 > s2, zero if s1 == s2
  */
@@ -41,9 +41,9 @@ int _strcmp(char *s1, char *s2)
 }
 
 /**
- * starts_with - checks if needle starts with haystack
- * @haystack: string to search
- * @needle: the substring to find
+ * _func - checks if needle starts with haystack
+ * haystack: string to search
+ * needle: the substring to find
  *
  * Return: address of next char of haystack or NULL
  */
@@ -56,9 +56,9 @@ char *starts_with(const char *haystack, const char *needle)
 }
 
 /**
- * _strcat - concatenates two strings
- * @dest: the destination buffer
- * @src: the source buffer
+ * _func - concatenates two strings
+ * dest: the destination buffer
+ * src: the source buffer
  *
  * Return: pointer to destination buffer
  */
